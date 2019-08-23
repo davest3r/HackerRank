4152d28fc8f5ae25ff8479c021803a4ff68f1e28
davest3r <dkhuang@gmail.com>
2019-08-23 11:37:54 -0700

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        scanner.close();
+
+        int r = 0;
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        while(n != 0) {
+            r = n % 2;
+            n = n / 2;
+            list.add(r);
+        }
+
+        int max = 0;
+        int counter = 0;
+
+        for(int i = 0; i < list.size(); i++) {
+            if(list.get(i) == 1) {
+                counter++;
+                if(counter > max) {
+                    max = counter;
+                }
+            } else {
+                counter = 0;
+            }
+        }
+
+        System.out.println(max);
+    }
+}
